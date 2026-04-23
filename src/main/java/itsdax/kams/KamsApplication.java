c16483dfcf98f1c332b575999af00266043d8dfe
Jermanus peter <133773488+jermanus74@users.noreply.github.com>
2026-04-23 03:10:06 +0300

    initialize project structure with Spring Boot, PostgreSQL, and Maven wrapper

---
+++ src/main/java/itsdax/kams/KamsApplication.java
@@ -0,0 +1,13 @@
+package itsdax.kams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KamsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/itsdax/kams/KamsApplicationTests.java
@@ -0,0 +1,13 @@
+package itsdax.kams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KamsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
